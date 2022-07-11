fbc8679223e4a53e40d1ea471183e21da9ee579a
Jatin Singh <71443622+Jatin-7@users.noreply.github.com>
2022-07-11 19:16:07 +0530

    Time: 1 ms (71.99%), Space: 42.1 MB (30.06%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -0,0 +1,29 @@
+class Solution {
+    
+        public static List<Integer> toList(int arr[]){
+            List<Integer> l = new ArrayList<>();
+            for(int i : arr){
+                l.add(i);
+            }
+            return l;
+        }
+    
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        int arr[][] = new int[numRows][];
+        
+        for(int i = 0; i<numRows; i++){
+            arr[i] = new int[i+1];
+            for(int j = 0; j<=i; j++){
+                if(j==0 || j==i){
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = arr[i-1][j] + arr[i-1][j-1];
+                }        
+            }
+            List l = toList(arr[i]);
+            list.add(l);
+        }
+        return list;
+    }
+}
