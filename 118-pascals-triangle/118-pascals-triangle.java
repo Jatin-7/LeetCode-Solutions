a0d5233dd2773af83b355231b846650d6deb9642
Jatin Singh <71443622+Jatin-7@users.noreply.github.com>
2022-07-11 19:27:34 +0530

    Time: 1 ms (71.99%), Space: 42.9 MB (5.37%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,46 +1,46 @@
 class Solution {
     
-        // public static List<Integer> toList(int arr[]){
-        //     List<Integer> l = new ArrayList<>();
-        //     for(int i : arr){
-        //         l.add(i);
-        //     }
-        //     return l;
-        // }
+    public static List<Integer> toList(int arr[]){
+        List<Integer> l = new ArrayList<>();
+        for(int i : arr){
+            l.add(i);
+        }
+        return l;
+    }
     
     public List<List<Integer>> generate(int numRows) {
         if(numRows == 0) return new ArrayList<>();
         List<List<Integer>> list = new ArrayList<List<Integer>>();
-//         int arr[][] = new int[numRows][];
-        
-//         for(int i = 0; i<numRows; i++){
-//             arr[i] = new int[i+1];
-//             for(int j = 0; j<=i; j++){
-//                 if(j==0 || j==i){
-//                     arr[i][j] = 1;
-//                 } else {
-//                     arr[i][j] = arr[i-1][j] + arr[i-1][j-1];
-//                 }        
-//             }
-//             List l = toList(arr[i]);
-//             list.add(l);
-//         }
-//         System.gc();
-//         return list;
+        int arr[][] = new int[numRows][];
         
         for(int i = 0; i<numRows; i++){
-            List<Integer> temp = new ArrayList<>();
+            arr[i] = new int[i+1];
             for(int j = 0; j<=i; j++){
                 if(j==0 || j==i){
-                    temp.add(1);
-                }
-                else{
-                    temp.add(list.get(i-1).get(j-1) + list.get(i-1).get(j));
-                }
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = arr[i-1][j] + arr[i-1][j-1];
+                }        
             }
-            list.add(temp);
+            List l = toList(arr[i]);
+            list.add(l);
         }
         System.gc();
         return list;
     }
 }
+        //Same logic can be implemented using a temp List
+        // for(int i = 0; i<numRows; i++){
+        //     List<Integer> temp = new ArrayList<>();
+        //     for(int j = 0; j<=i; j++){
+        //         if(j==0 || j==i){
+        //             temp.add(1);
+        //         }
+        //         else{
+        //             temp.add(list.get(i-1).get(j-1) + list.get(i-1).get(j));
+        //         }
+        //     }
+        //     list.add(temp);
+        // }
+        // System.gc();
+        // return list;
