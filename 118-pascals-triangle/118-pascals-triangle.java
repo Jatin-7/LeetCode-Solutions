8ccc850f11b25a334be99d272d3f2d6a7eced2aa
Jatin Singh <71443622+Jatin-7@users.noreply.github.com>
2022-07-11 19:28:22 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (83.23%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -29,18 +29,3 @@ class Solution {
         return list;
     }
 }
-        //Same logic can be implemented using a temp List
-        // for(int i = 0; i<numRows; i++){
-        //     List<Integer> temp = new ArrayList<>();
-        //     for(int j = 0; j<=i; j++){
-        //         if(j==0 || j==i){
-        //             temp.add(1);
-        //         }
-        //         else{
-        //             temp.add(list.get(i-1).get(j-1) + list.get(i-1).get(j));
-        //         }
-        //     }
-        //     list.add(temp);
-        // }
-        // System.gc();
-        // return list;
