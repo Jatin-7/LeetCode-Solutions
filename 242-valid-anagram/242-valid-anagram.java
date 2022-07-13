4cba5a16fbea4803b641100672ca1deb462c562e
Jatin Singh <71443622+Jatin-7@users.noreply.github.com>
2022-07-13 23:57:44 +0530

    Time: 35 ms (9.60%), Space: 46.8 MB (12.94%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        Map<Character, Integer> map1 = new HashMap<Character, Integer>();
+        Map<Character, Integer> map2 = new HashMap<Character, Integer>();
+        for(int i = 0; i<s.length(); i++){
+            char tmp1 = s.charAt(i);
+            map1.put(tmp1, map1.getOrDefault(tmp1, 0)+1);
+        }
+        for(int i = 0; i<t.length(); i++){
+            char tmp2 = t.charAt(i);
+            map2.put(tmp2, map2.getOrDefault(tmp2, 0)+1);
+        }
+        
+        return map1.equals(map2);
+    }
+}
