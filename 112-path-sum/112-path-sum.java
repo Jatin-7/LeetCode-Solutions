5a668cae0c8664a545563e826429743540857184
Jatin Singh <71443622+Jatin-7@users.noreply.github.com>
2022-07-19 13:14:43 +0530

    Time: 1 ms (60.82%), Space: 44.4 MB (6.47%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -16,35 +16,30 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         
-        if(root == null){
+         if(root == null){
             return false;
         }
         
-        if(root.left == null && root.right == null && root.val == targetSum){
-            return true;
-        }
-        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val); 
-        
-        
-//         Queue<TreeNode> nodes = new LinkedList<>();
-//         nodes.add(root);
-//         while(!nodes.isEmpty()) {
-//             TreeNode curr = nodes.poll();
-//             if(curr.left == null && curr.right == null){
-//                 if(targetSum == curr.val){
-//                     return true;
-//                 }
-//             }
+        Queue<TreeNode> nodes = new LinkedList<>();
+        nodes.add(root);
+        while(!nodes.isEmpty()) {
+            TreeNode curr = nodes.poll();
+            if(curr.left == null && curr.right == null){
+                if(targetSum == curr.val){
+                    return true;
+                }
+            }
             
-//             if(curr.left != null) {
-//                 curr.left.val = curr.left.val + curr.val;
-//                 nodes.add(curr.left);
-//             }
-//             if(curr.right != null) {
-//                 curr.right.val = curr.right.val + curr.val;
-//                 nodes.add(curr.right);
-//             }
-//         }
-//         return false;
+            if(curr.left != null) {
+                curr.left.val = curr.left.val + curr.val;
+                nodes.add(curr.left);
+            }
+            if(curr.right != null) {
+                curr.right.val = curr.right.val + curr.val;
+                nodes.add(curr.right);
+            }
+        }
+        return false;
+        
     }
 }
